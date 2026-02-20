cbcc9033162d5d2f4c3572a934554f93830c43c7
Lostl12 <lost02401@gmail.com>
2026-02-20 03:42:24 +0100

    Refactor twitter function error handling

--- extractors/twitter.js
+++ extractors/twitter.js
@@ -2,12 +2,9 @@ import fetch from "node-fetch";
 
 export default async function twitter(url) {
   try {
-    // Use public API service or parsing for simplicity
     const apiUrl = `https://api.twitter.com/2/tweet?url=${encodeURIComponent(url)}`;
     const res = await fetch(apiUrl);
     const data = await res.json();
     return { thumbnail: data.thumbnail, size: "Unknown", qualities: [{ name: "HD", url: data.video_url }] };
-  } catch {
-    return null;
-  }
+  } catch { return null; }
 }
