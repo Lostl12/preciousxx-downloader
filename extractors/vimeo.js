907e8c3a105085aa192c70703ea8460c45bae573
Lostl12 <lost02401@gmail.com>
2026-02-20 03:44:00 +0100

    Simplify error handling in Vimeo extractor

--- extractors/vimeo.js
+++ extractors/vimeo.js
@@ -5,7 +5,5 @@ export default async function vimeo(url) {
     const res = await fetch(`https://vimeo.com/api/oembed.json?url=${encodeURIComponent(url)}`);
     const data = await res.json();
     return { thumbnail: data.thumbnail_url, size: "Unknown", qualities: [{ name: "HD", url }] };
-  } catch {
-    return null;
-  }
+  } catch { return null; }
 }
