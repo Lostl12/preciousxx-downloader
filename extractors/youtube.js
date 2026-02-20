f652a029f50a5c840dc974bd5328973743390037
Lostl12 <lost02401@gmail.com>
2026-02-20 03:41:50 +0100

    Simplify error handling in YouTube extractor

--- extractors/youtube.js
+++ extractors/youtube.js
@@ -6,7 +6,5 @@ export default async function youtube(url) {
     const formats = ytdl.filterFormats(info.formats, "videoandaudio");
     const hdFormat = formats.find(f => f.qualityLabel === "720p") || formats[0];
     return { thumbnail: info.videoDetails.thumbnails.pop().url, size: "Unknown", qualities: [{ name: hdFormat.qualityLabel, url: hdFormat.url }] };
-  } catch {
-    return null;
-  }
+  } catch { return null; }
 }
