415d8270d168685784112f8e2b7b8d50558e6f79
Lostl12 <lost02401@gmail.com>
2026-02-20 03:44:34 +0100

    Simplify error handling in dailymotion extractor

--- extractors/dailymotion.js
+++ extractors/dailymotion.js
@@ -5,7 +5,5 @@ export default async function dailymotion(url) {
     const res = await fetch(`https://api.dailymotion.com/video/${url.split("/video/")[1]}?fields=thumbnail_url,url`);
     const data = await res.json();
     return { thumbnail: data.thumbnail_url, size: "Unknown", qualities: [{ name: "HD", url: data.url }] };
-  } catch {
-    return null;
-  }
+  } catch { return null; }
 }
