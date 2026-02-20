12e3819e94154437ef97461a25ce16de002afe3b
Lostl12 <lost02401@gmail.com>
2026-02-20 02:20:22 +0100

    Add files via upload

---
+++ extractors/vimeo.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+
+module.exports = async function(url) {
+  const api = `https://api.akuari.my.id/vimeo?url=${encodeURIComponent(url)}`;
+  const response = await axios.get(api);
+
+  if (!response.data || !response.data.result) return null;
+
+  const media = response.data.result;
+
+  const qualities = media.map((item) => ({
+    quality: item.quality || "HD",
+    url: `/getfile?url=${encodeURIComponent(item.url)}`
+  }));
+
+  return {
+    thumbnail: media[0].thumbnail || "",
+    size: `${media.length} file(s)`,
+    qualities
+  };
+};
