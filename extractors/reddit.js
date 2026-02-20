fbb3d065b0a7f8e9809874a58c3b3b6e1a56aa03
Lostl12 <lost02401@gmail.com>
2026-02-20 03:29:08 +0100

    Refactor Reddit extractor to use node-fetch

--- extractors/reddit.js
+++ extractors/reddit.js
@@ -1,21 +1,14 @@
-const axios = require("axios");
+import fetch from "node-fetch";
 
-module.exports = async function(url) {
-  const api = `https://api.akuari.my.id/reddit?url=${encodeURIComponent(url)}`;
-  const response = await axios.get(api);
-
-  if (!response.data || !response.data.result) return null;
-
-  const media = response.data.result;
-
-  const qualities = media.map((item) => ({
-    quality: "HD",
-    url: `/getfile?url=${encodeURIComponent(item.url)}`
-  }));
-
-  return {
-    thumbnail: media[0].thumbnail || "",
-    size: `${media.length} file(s)`,
-    qualities
-  };
-};
+export default async function reddit(url) {
+  try {
+    const res = await fetch(`${url}.json`);
+    const data = await res.json();
+    const post = data[0].data.children[0].data;
+    const videoUrl = post.secure_media?.reddit_video?.fallback_url || "";
+    const thumbnail = post.thumbnail || "";
+    return { thumbnail, size: "Unknown", qualities: [{ name: "HD", url: videoUrl }] };
+  } catch {
+    return null;
+  }
+}
